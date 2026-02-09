32ddfa9e94ab955b7fca0592dec74c792bb784dd
benha <benha@LAPTOP-09ER5QNJ>
2026-02-09 18:40:03 +0100

    zedt hassen

--- projectNetflix/src/main/java/groupna/projectNetflix/entities/Serie.java
+++ projectNetflix/src/main/java/groupna/projectNetflix/entities/Serie.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 public class Serie extends Oeuvre{
 	private Map<Integer,List<Episode>> saisons;
+	private int hassen;
 
 	public Serie(String resume, Categorie cat, String titre, LocalDate dateDeSortie, List<Artiste> acteurs,
 			List<Artiste> directeurs, double rate, Map<Integer, List<Episode>> saisons) {
